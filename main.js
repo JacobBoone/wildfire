92dc419aa5ff20104ff6b9d17d5b2f183d825d8f
Jacob <jakeboone@aol.com>
2014-07-12 10:02:03 -0600

    solution

--- main.js
+++ main.js
@@ -44,11 +44,62 @@ $('.power').click(function(){
 
 
 
+	var run = function() {
+		$('#land .btn-danger').each(function() {
+			var next = $(this).parent().next().children().eq(0);
+			var prev = $(this).parent().prev().children().eq(0);
+			burnhot(next);
+			burnhot(prev);
+		});
 
+		$('#land .btn-warning').each(function() {
+			var next = $(this).parent().next().children().eq(0);
+			var next2 = $(this).parent().next().next().children().eq(0);
+			var prev = $(this).parent().prev().children().eq(0);
+			var prev2 = $(this).parent().prev().prev().children().eq(0);
+			burnwarm(next);
+			burnwarm(prev);
 
+			if(!isWater(next)) {
+				burnwarm(next2);
+			}
 
+			if(!isWater(prev)) {
+				burnwarm(prev2);
+			}
+		});
+	};
 
+	var burnwarm = function(el) {
+		if(!isWater(el) && !isHot(el)) {
+			el.addClass('btn-warning');
+		}
+	};
 
+	var burnhot = function(el) {
+		if(!isWater(el)) {
+			if(isWarm(el)) {
+				el.addClass('btn-danger');
+			}
+			else {
+				el.addClass('btn-warning');
+			}
+		}
+	};
+
+	var isWater = function(el) {
+		return el.hasClass('btn-info');
+	};
+
+	var isHot = function(el) {
+		return el.hasClass('btn-danger');
+	};
+
+	var isWarm = function(el) {
+		return el.hasClass('btn-warning');
+	};
+
+	setInterval(run, 1000);
 
 
 
